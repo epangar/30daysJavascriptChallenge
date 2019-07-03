6e619feec8c2ea14c009a50501fdbe271d33fd84
Estanislao Pan García <epan@indra.es>
2019-07-03 16:00:57 +0200

    Ejercicio 1, javascript

---
+++ 01 - JavaScript Drum Kit/drums.js
@@ -0,0 +1,19 @@
+
+
+function valueKey(){
+    
+    var value=document.getElementsByClassName("key")
+    
+    return value
+}
+
+function clickKey(){
+
+}
+
+(function drums(){
+    
+    window.addEventListener('onkeyup', valueKey)
+    
+    
+})()
