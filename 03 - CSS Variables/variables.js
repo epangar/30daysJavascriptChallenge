a1d1baea20c56e7947c997789e8aadae0500bd0f
Estanislao Pan García <epan@indra.es>
2019-07-22 15:33:19 +0200

    Variables 03

--- 03 - CSS Variables/variables.js
+++ 03 - CSS Variables/variables.js
@@ -1,3 +1,8 @@
 (()=>{
+  let inputs= document.querySelectorAll('.controls input');
   
+  function handleUpdate() {
+    console.log(this.dataset.sizing)
+  }
+  inputs.forEach(input => input.addEventListener('change', handleUpdate))
 })()
