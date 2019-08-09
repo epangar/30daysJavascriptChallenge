1f4956fe677478079573ed3ad76ee314147360d1
Estanislao Pan García <epan@indra.es>
2019-08-09 15:45:53 +0200

    Exercise 03 Javascript

--- 03 - CSS Variables/variables.js
+++ 03 - CSS Variables/variables.js
@@ -1,9 +1,10 @@
 (()=>{
-  let inputs= document.querySelectorAll('.controls input');
+  const inputs= document.querySelectorAll('.controls input');
   
   function handleUpdate() {
     console.log(this.dataset.sizing)
   }
   
   inputs.forEach(input => input.addEventListener('change', handleUpdate))
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
 })()
