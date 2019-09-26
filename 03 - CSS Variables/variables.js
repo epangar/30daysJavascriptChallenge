f9bb0bbc02d1178b1fa31846d7af19753b54ba7c
Tanis Pan <e.pan.gar@gmail.com>
2019-09-26 23:00:15 +0200

    Exercise 3

--- 03 - CSS Variables/variables.js
+++ 03 - CSS Variables/variables.js
@@ -1,10 +1,9 @@
 window.onload=()=>{
   const inputs= document.querySelectorAll('.controls input');
   
-  function handleUpdate() {
-    
+  let handleUpdate = () => {
     const suffix = this.dataset.sizing || "";
-    console.log(suffix)
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
   }
   
   inputs.forEach(input => input.addEventListener('change', handleUpdate))
