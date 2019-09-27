61054f28702663eb5f948fd9461b5f338083a537
Estanislao Pan García <epan@indra.es>
2019-09-27 14:59:23 +0200

    Type Ahead

--- 06 - Type Ahead/type.js
+++ 06 - Type Ahead/type.js
@@ -1,19 +1,26 @@
+let cities = [];
+
 window.onload = () => {
     const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-    let cities=[];
     
+
     fetch(endpoint)
-        .then((response)=>response.json())
-        .then((myJson)=>{
-            cities=myJson.map(city=>city);
-            console.table(cities);
+        .then((response) => response.json())
+        .then((myJson) => {
+            cities = myJson.map(city => city);
+            
         });
+        
     
     
-}
-
-let fillCities =()=>{
 
 }
+const myInput = document.querySelector(".search");
+    console.log(myInput);
+myInput.addEventListener('input', searchCities);
 
+function searchCities(){
+    let query=this.value;
+    console.log(query);
+}
