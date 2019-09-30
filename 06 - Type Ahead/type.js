a9aef5f54a9df1859d90df12398bb4e48d2b6df6
Tanis Pan <e.pan.gar@gmail.com>
2019-09-30 20:21:34 +0200

    Testing

--- 06 - Type Ahead/type.js
+++ 06 - Type Ahead/type.js
@@ -1,26 +1,31 @@
-let cities = [];
+
 
 window.onload = () => {
     const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-    
+    let cities = [];
 
     fetch(endpoint)
         .then((response) => response.json())
         .then((myJson) => {
-            cities = myJson.map(city => city);
+                cities = myJson.map(function(city){
+                  return city;
+                });
+            });
             
-        });
+            
         
     
-    
-
-}
-const myInput = document.querySelector(".search");
+    const myInput = document.querySelector(".search");
     console.log(myInput);
-myInput.addEventListener('input', searchCities);
+    myInput.addEventListener('input', searchCities);
 
-function searchCities(){
-    let query=this.value;
-    console.log(query);
+    function searchCities(){
+        let query=this.value;
+        console.log(query);
+        //console.log(cities);
+        let filtered=cities.map(city=>Object.values(city))
+                            //.filter(city=>city.includes("a"))
+        console.log(filtered)
+    }
 }
