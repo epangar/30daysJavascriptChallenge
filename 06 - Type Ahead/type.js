74a0ad208db4581cb845d57d0d66dd8e7cdbf2a8
Estanislao Pan García <epan@indra.es>
2019-09-30 18:47:23 +0200

    City Arrays

--- 06 - Type Ahead/type.js
+++ 06 - Type Ahead/type.js
@@ -1,26 +1,63 @@
-let cities = [];
-
 window.onload = () => {
     const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-    
+    const cities = [],
+    myInput = document.querySelector(".search");
 
     fetch(endpoint)
-        .then((response) => response.json())
-        .then((myJson) => {
-            cities = myJson.map(city => city);
-            
-        });
+    .then((response) => response.json())
+    .then((myJson) => {
+        cities.push(...myJson);
+    });
+    
+    let searchCities = () =>{
         
-    
-    
+        const query=myInput.value,        
+        expression = new RegExp(query, 'gi'),
+        
+        filtered = cities.filter(element =>
+            element.city.match(expression) || element.state.match(expression));
+        
 
-}
-const myInput = document.querySelector(".search");
-    console.log(myInput);
-myInput.addEventListener('input', searchCities);
+        
+            
+        buildList(filtered)
+    }
 
-function searchCities(){
-    let query=this.value;
-    console.log(query);
+    let buildList = (array) =>{
+        const list = document.querySelectorAll('.suggestions')[0];
+
+
+        if(list.hasChildNodes()){
+            emptyList(list)
+        }
+        
+        array.forEach(place=>{
+            let LI =document.createElement('LI');
+            let cityName= document.createTextNode([
+                place.city,
+                " "+place.state
+                
+            ]);
+            let population = document.createTextNode(" "+place.population)
+            
+            LI.appendChild(cityName);
+            LI.appendChild(population);
+            list.appendChild(LI);
+
+        })
+
+
+    }
+
+    let emptyList = (element) => {
+        
+        while(element.hasChildNodes()){
+
+            element.removeChild(element.childNodes[0])
+        }
+    }
+    
+    myInput.addEventListener('input', searchCities);
 }
+
