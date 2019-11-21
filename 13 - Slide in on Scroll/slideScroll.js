b3a8d40d286c9452695b1aafa96c471378d7c7d7
Estanislao Pan García <epan@indra.es>
2019-11-21 17:58:15 +0100

    Haciendo ejercicio 13

--- 13 - Slide in on Scroll/slideScroll.js
+++ 13 - Slide in on Scroll/slideScroll.js
@@ -1,14 +1,39 @@
+// let  debounce = (func, wait = 20, immediate = true) => {
+//   let timeout;
+//   return function() {
+//     let context = this, args = arguments;
+//     let later = function() {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     let callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
+
+
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-  }
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+let checkSlide = (e)=>{
+  console.count(e);
+  slides.forEach(slide=> console.log(slide))
+}
+
+const slides = document.querySelectorAll('.slide-in');
+
+window.addEventListener('scroll', debounce(checkSlide))
