982d8122dca1ca0d3f15e62a82bba3d4205f7fea
Estanislao Pan García <epan@indra.es>
2019-11-22 11:31:33 +0100

    Configurando GitHub pages

--- loadIframe.js
+++ loadIframe.js
@@ -6,7 +6,10 @@ window.onload=()=>{
         console.clear();
 
         const value = this.value;
+        
         iframe[0].attributes[4].value = `./${value}/index-START.html`
+        console.log(value)
+        console.log(iframe[0].attributes[4].value)
     }
 
     inputs.forEach(input => {
