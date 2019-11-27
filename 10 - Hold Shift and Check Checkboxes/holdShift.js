e2e900fa92408ea1496121a9b65a8f50774ba0a1
Tanis Pan <e.pan.gar@gmail.com>
2019-11-27 21:05:30 +0100

    10 - Javascript

--- 10 - Hold Shift and Check Checkboxes/holdShift.js
+++ 10 - Hold Shift and Check Checkboxes/holdShift.js
@@ -1,22 +1,22 @@
 let detectPressed = (pressedKey) =>{
   
   let value = pressedKey.key;
-  console.log(value);
-
+  
   if(value==='Shift'){
-    window.addEventListener('repeat', detectHold(value));
+    
   }
   
 }
 
 let detectHold =(pressedKey)=>{
-  
+  debugger
   console.log(`${pressedKey} is hold.`);
 }
 
 window.onload = (()=>{
-
-  
+  var checkbox = document.querySelectorAll('.inbox input')
+  console.log(checkbox)
 })
 
 window.addEventListener('keydown', detectPressed);
+window.addEventListener('keydown', (event)=> event.repeat ? detectHold : "");
