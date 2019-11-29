dc14638a5bda82e9757d5b37047809bd98766354
Estanislao Pan García <epan@indra.es>
2019-11-29 09:13:27 +0100

    10 - everything checks

--- 10 - Hold Shift and Check Checkboxes/holdShift.js
+++ 10 - Hold Shift and Check Checkboxes/holdShift.js
@@ -1,46 +1,42 @@
-//window.onload = (()=>{
-  let isShift = (event) => event.shiftKey;
+let isShift = (event) => event.shiftKey;
 
-  let checkbox = document.querySelectorAll('.inbox input')
-  
-  let detectPressed = (event) =>{
+let checkbox = document.querySelectorAll('.inbox input')
 
-    if(isShift(event)){
-       detectPortion();
-    }
-  
+let detectPressed = (event) => {
+
+  if (isShift(event)) {
+    detectPortion();
   }
 
-  let detectPortion = () =>{
-    
-    let startAndEnd=[];
+}
 
-    checkbox.forEach((o,p)=>{
-      if(o.checked){
-        startAndEnd.push(p);
+let detectPortion = () => {
+
+  let startAndEnd = [];
+
+  checkbox.forEach((o, p) => {
+    if (o.checked) {
+      startAndEnd.push(p);
+    }
+  })
+
+
+  if (startAndEnd.length > 1) {
+
+    let start = startAndEnd[0];
+
+    let end = startAndEnd[startAndEnd.length - 1]
+
+    checkbox.forEach((o, p) => {
+      if (p > start && p <= end) {
+        o.checked = true;
       }
     })
 
-
-    if(startAndEnd.length>1){
-      
-      let start=startAndEnd[0];
-
-      let end=startAndEnd[startAndEnd.length-1]
-
-      checkbox.forEach((o,p)=>{
-        if(p>start && p<=end){
-          o.checked=true;
-        }
-      })
-
-    }
-
   }
 
+}
 
 
-  window.addEventListener('keydown', detectPressed);
 
-
-//})
+window.addEventListener('keydown', detectPressed);
