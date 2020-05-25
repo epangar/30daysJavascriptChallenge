29cb49b891a71180965c68ceef591f9ab4c1e971
Estanislao Pan García <epan@indra.es>
2020-05-25 20:30:43 +0200

    Exercise 11

--- 11 - Custom Video Player/videoPlayer.js
+++ 11 - Custom Video Player/videoPlayer.js
@@ -1,5 +1,23 @@
-const player = document.querySelector('.player')
-const viewer = player.querySelector('.viewer')
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
-console.log(player)
-console.log(viewer)
+const togglePlay = () =>  video.paused ? video.play() : video.pause();
+
+const updateButton = () => {
+    const icon = video.paused ? "▶" : "❚❚" ;
+    toggle.textContent = icon;
+}
+
+const skip = () => {
+
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
