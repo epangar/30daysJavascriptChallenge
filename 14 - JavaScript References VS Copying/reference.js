0f814be817bdb601a1e5a5c3c5caac8e4469acb8
Estanislao Pan García <epan@indra.es>
2020-06-04 22:14:50 +0200

    Basics exercise 14

--- 14 - JavaScript References VS Copying/reference.js
+++ 14 - JavaScript References VS Copying/reference.js
@@ -1,37 +1,64 @@
 // start with strings, numbers and booleans
 
-    // Let's say we have an array
-    const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
 
-    // and we want to make a copy of it.
+let name = 'Wes';
+let name2 = name;
+console.log(name, name2);
+name = 'wesley';
+console.log(name, name2);
 
-    // You might think we can just do something like this:
+// Let's say we have an array
+const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
-    // however what happens when we update that array?
+// and we want to make a copy of it.
+const team = players;
 
-    // now here is the problem!
+const copy = players.map(e=>e)
 
-    // oh no - we have edited the original array too!
 
-    // Why? It's because that is an array reference, not an array copy. They both point to the same array!
+// You might think we can just do something like this:
 
-    // So, how do we fix this? We take a copy instead!
+// however what happens when we update that array?
+// team[3] = 'Lux';
 
-    // one way
+// now here is the problem!
 
-    // or create a new array and concat the old one in
+// oh no - we have edited the original array too!
 
-    // or use the new ES6 Spread
+// Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
-    // now when we update it, the original one isn't changed
+// So, how do we fix this? We take a copy instead!
 
-    // The same thing goes for objects, let's say we have a person object
 
-    // with Objects
-    const person = {
-      name: 'Wes Bos',
-      age: 80
-    };
+// one way
+const team2 = players.slice()
+
+// or create a new array and concat the old one in
+const team3 = [].concat(players)
+
+// or use the new ES6 Spread
+const team4 = [...players]
+
+// now when we update it, the original one isn't changed
+
+console.log(`players = ${players}`)
+console.log(`copy = ${copy}`)
+console.log(`team2 = ${team2}`)
+console.log(`team3 = ${team3}`)
+console.log(`team4 = ${team4}`)
+
+// The same thing goes for objects, let's say we have a person object
+
+// with Objects
+const person = {
+  name: 'Wes Bos',
+  age: 80
+};
 
     // and think we make a copy:
 
