0f8326a0e073f236da56e91884cbc314b962f946
Estanislao Pan García <epan@indra.es>
2020-06-05 01:07:05 +0200

    Exercise 14 finished

--- 14 - JavaScript References VS Copying/reference.js
+++ 14 - JavaScript References VS Copying/reference.js
@@ -37,13 +37,13 @@ const copy = players.map(e=>e)
 
 // one way
 const team2 = players.slice()
-
+team2[1]=2
 // or create a new array and concat the old one in
 const team3 = [].concat(players)
-
+team3[2]=3
 // or use the new ES6 Spread
 const team4 = [...players]
-
+team4[3]=4
 // now when we update it, the original one isn't changed
 
 console.log(`players = ${players}`)
@@ -61,9 +61,30 @@ const person = {
 };
 
     // and think we make a copy:
+    // const captain = person;
+    // captain.number = 99;
 
     // how do we take a copy instead?
+    const cap2 = Object.assign({}, person, {number : 99}) 
+    console.log(cap2)
+
 
     // We will hopefully soon see the object ...spread
+    //const cap3 = {...person}
 
     // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+    const cap4 = {
+      name: 'Wes',
+      age: 100,
+      social: {
+        twitter: '@wesbos',
+        facebook: 'wesbos.developer'
+      }
+    };
+
+    console.log(cap4)
+    const dev = Object.assign({}, cap4)
+
+    const dev2 = JSON.parse(JSON.stringify(cap4));
+
