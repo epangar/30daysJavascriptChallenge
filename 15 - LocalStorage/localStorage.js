89f68f5000508c564cf177e62a20d95602a02ca5
Estanislao Pan García <epan@indra.es>
2020-11-08 19:14:58 +0100

    addItem function added

--- 15 - LocalStorage/localStorage.js
+++ 15 - LocalStorage/localStorage.js
@@ -1,3 +1,23 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+
+function addItem(element){
+    console.log("Added")
+    element.preventDefault()
+    debugger
+    const text = (element.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    }
+
+    items.push(item);
+    this.reset();
+};
+
+
+
+
+addItems.addEventListener('submit', addItem)
